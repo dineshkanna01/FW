313c428f10e818898ec41770cb48dd259509b913
dineshkanna01 <dinesh.kanna@igtsolution.com>
2021-10-28 11:51:36 +0530

    Updated

--- src/main/java/cmdprompt/SyncPipe.java
+++ src/main/java/cmdprompt/SyncPipe.java
@@ -6,11 +6,11 @@ import java.io.PrintStream;
 public class SyncPipe implements Runnable {
 
 	public SyncPipe(InputStream errorStream, PrintStream err) {
-		// TODO Auto-generated constructor stub
+
 	}
+	
 
 	public void run() {
-		// TODO Auto-generated method stub
 
 	}
 
